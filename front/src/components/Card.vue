349a03d5b366a7983f390325c436f5b9dab06789
Polegss <andre.jucovski@usp.br>
2020-11-22 22:14:32 -0300

    css cards

--- front/src/components/Card.vue
+++ front/src/components/Card.vue
@@ -1,22 +1,29 @@
 <template>  
-    <div class="col-sm-3">
+    <div class="col-xl-3 col-md-6">
       <div class="card">
-        <img :src="'/imagens/abacate.jpg'" class="card-img-top" v-bind:alt="produto.nome" />
-        <div class="card-body">
-          <h3 class="card-title">{{ produto.nome }}</h3>
-          <h5>{{ produto.preco }} {{ produto.descricao }}</h5>
-
-          <div id="buttons" class="but">
-            <div align="center">
-              <button v-on:click="removerQuantidade()" class="btn btn-md btn-outline-dark inline-block-child" style="margin-right: 2%;">
-                <span>-</span>
-              </button>
-              <button class="btn btn-md btn-outline-dark inline-block-child"> {{ quantidade }} </button>
-              <button v-on:click="adicionarQuantidade()" class="btn btn-md btn-outline-dark inline-block-child" style="margin-left: 2%;">
-                <span><i class="fa fa-arrow-right"></i>+</span>
-              </button>
-            </div>
+        <img :src="'/imagens/ABACATE.jpg'" class="card-img-top cardimagem" v-bind:alt="produto.nome" />
+        <div class="card-body cardfundo">
+          <h3 class="card-title cardtitle">{{ produto.nome }}</h3>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col" style="padding: 0px 5px">
+                <h5 class="precocard" style="font-family: Montserrat">{{ produto.preco }} {{ produto.descricao }}</h5>
+              </div>
+              <div class="col-6">
+                <div id="buttons">
+                  <div align="center">
+                    <button v-on:click="removerQuantidade()" class="btn btn-sm btn-outline-dark inline-block-child">
+                      <span>-</span>
+                    </button>
+                    <button class="btn btn-sm btn-outline-dark inline-block-child"> {{ quantidade }} </button>
+                    <button v-on:click="adicionarQuantidade()" class="btn btn-sm btn-outline-dark inline-block-child">
+                      <span><i class="fa fa-arrow-right"></i>+</span>
+                    </button>
+                </div>
+              </div>
+            </div>             
           </div>
+        </div>
         </div>
       </div>
     </div>
@@ -99,5 +106,39 @@ export default {
 </script>
 
 <style scoped>
+.inline-block-child{
+    display: inline-block;
+  }
+.precocard{
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 15px;
+  text-align: center;
+  margin-top: 8px;
+  color: #AF7061;
+}
+.cardtitle{
+font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 10px;
+text-align: center;
 
+color: #AF7061;
+}
+.card{
+  background-color: rgba(196, 196, 196, 0.7);
+  border-radius: 10px;
+}
+.cardfundo{
+  background: rgba(196, 196, 196, 0);
+  border-radius: 10px;
+}
+.cardimagem{
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+border-radius: 10px;
+}
 </style>
