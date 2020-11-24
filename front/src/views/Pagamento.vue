4f132c44faecfb173de2d7003b4577e5af1ce050
yancr22 <yanrohwedder@gmail.com>
2020-11-24 13:33:42 -0300

    Modifica a tela de pagamento

--- front/src/views/Pagamento.vue
+++ front/src/views/Pagamento.vue
@@ -1,125 +1,42 @@
 <template>
-  <div class="cadastro">
+  <div class="pagamento">
     <div class="container">
 
-    <div class="row">
-        <div class="shape">
-            <h2 class="shapecontent">Escolha a forma de pagamento</h2>
+        <div class="row">
+            <div class="shape">
+                <h2 class="shapecontent">FINALIZANDO COMPRA</h2>
+            </div>
         </div>
-    </div>
 
 
         <form name="pagamento" class="form-horizontal" method="POST" onsubmit="event.preventDefault();">
 
             <fieldset>
-                <legend>DADOS PESSOAIS</legend>
+                <legend>CONFIRME LOCAL DE ENTREGA</legend>
 
-                <div class="form-group">
-                    <label class="control-label" for="nome">NOME*</label>
-                    <input type="text" id="nome" class="form-control" size="80" name="user_nome" required>
+                <div v-for="(local,index) in enderecos" :key="index" class="form-group">
+                    <input type="radio" v-bind:id="key" v-bind:value="key" name="Entrega" required/>
+                    <label v-bind:for="key">{{ local.Cidade +", "+ local.rua +" "+ local.Num +" "+ local.Complemento }}</label>
                 </div>
-
-                <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="cpf">CPF*</label>
-                        <input id="cpf" type="text" name="user_cpf" placeholder="Ex.: 000.000.000-00" maxlength="14" class="form-control" pattern="[0-9]{2-3}.[0-9]{3}.[0-9]{3}-[0-9]{2}" onBlur="showhide()" required/>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label for="rg" class="control-label">RG*</label>
-                        <input id="rg" type="text" name="user_rg" placeholder="Ex.: 00.000.000-0" maxlength="14" class="form-control" pattern="[0-9]{2-3}.[0-9]{3}.[0-9]{3}-[0-9]{2}" onBlur="showhide()" required/>
-                    </div>
-                </div>
-
-                <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="tel">TELEFONE*</label>
-                        <input id="tel" type="tel" name="user_tel" placeholder="Ex.: 00 00000-0000" maxlength="15" class="form-control" pattern="[0-9]{2} [0-9]{4-5}-[0-9]{4}" onBlur="showhide()" required/>                    
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label for="Nascimento" class="control-label">NASCIMENTO*</label>
-                        <input id="Nascimento" type="date" name="user_Nascimento" class="form-control" onBlur="showhide()" required/>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="control-label" for="email">EMAIL*</label>
-                    <input id="email" type="email" name="user_email" class="form-control"  required/>
-                </div>
-
-                <div class="form-group">
-                    <label class="control-label" for="emailConf">CONFIRMAR EMAIL*</label>  
-                    <input id="email2" type="email" name="user_email2" class="form-control" size="50" required/>
-                </div>
-
             </fieldset>
             <hr class="my-4">
+
             <fieldset>
-                <legend>DADOS DE ENTREGA</legend>
+                <legend>DADOS DE PAGAMENTO</legend>
                 
-                <div class="form-group">
-                    <label for="rua" class="control-label">ENDEREÇO*</label>
-                    <input id="rua" type="text" name="user_rua" class="form-control"  required/>
-                </div>
-
+                <p>FORMA DE PAGAMENTO*</p>
                 <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="n">NÚMERO*</label>
-                        <input id="n" type="number" name="user_n" class="form-control" required/>
+                    <div class="form-check form-check-inline">
+                        <input id="dinheiro" type="radio" name="forma_pagamento" class="form-check-input" value="dinheiro" required/>
+                        <label for="dinheiro" class="form-check-label">Dinheiro</label>
                     </div>
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="compl">COMPLEMENTO*</label>
-                        <input id="compl" type="text" name="user_compl" class="form-control" required/>
-                    </div>
-                </div>
-
-                <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="cidade">CIDADE*</label>
-                        <input id="cidade" type="text" name="user_cidade" class="form-control" required/>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="bairro">BAIRRO*</label>
-                        <input id="bairro" type="text" name="user_bairro" class="form-control" required/>
-                    </div>
-                </div>
-
-                <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="est">ESTADO*</label>
-                        <input id="est" type="text" name="user_est"  maxlength="2" class="form-control" required/>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="cep">CEP*</label>
-                        <input id="cep" type="text" name="user_cep" class="form-control" placeholder="Ex.: 00000-000" maxlength="9" pattern="[0-9]{5}-[0-9]{3}" onBlur="showhide()" required/>
+                    <div class="form-check form-check-inline">
+                        <input id="cartao" type="radio" name="forma_pagamento" class="form-check-input" value="cartao" required/>
+                        <label for="cartao" class="form-check-label">Cartão</label>
                     </div>
                 </div>
             </fieldset>
             <hr class="my-4">
-
-            <fieldset>
-                <legend>DADOS DE LOGIN</legend>
-                <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="usuario">NOME DE USUÁRIO*</label>
-                        <input id="usuario" type="text" name="user_usuario"  class="form-control" required/>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="senha">SENHA*</label>
-                        <input id="senha" type="password" name="user_senha"  class="form-control" required/>
-                    </div>
-                </div>
-
-                <div class="form-row">
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="usuario2">CONFIRMAR NOME DE USUÁRIO*</label>
-                        <input id="usuario2" type="text" name="user_usuario2"  class="form-control" required/>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label class="control-label" for="senha2">CONFIRMAR SENHA*</label>
-                        <input id="senha2" type="password" name="user_senha2"  class="form-control" required/>
-                    </div>
-                </div>
-            </fieldset>
 
             <div id="outer">
                 <div class="row">
@@ -135,3 +52,24 @@
     </div>
   </div>
 </template>
+
+<script>
+
+export default {
+    name: 'Pagamento',
+    data: function() {
+        return{
+            enderecos: [
+                {index: 0, rua: "Rua Jacinto Favoretto", Num: "0", Complemento: "", Cidade: "Sao Carlos"}, 
+                {index: 1, rua: "Rua Luiz Vaz de Toledo Piza", Num: "0", Complemento: "Bloco 2, Apto 223", Cidade: "Sao Carlos"}
+            ]
+        }
+        
+    },
+}
+
+</script>
+
+<style scoped>
+
+</style>
