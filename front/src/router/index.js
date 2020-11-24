b86c9279daae34623dc05eee9ea1eb57e8655e1b
gicutieri <49662106+gicutieri@users.noreply.github.com>
2020-11-23 22:19:54 -0300

    Update index.js

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -40,6 +40,12 @@ const routes = [
     component: () => import("@/views/Cadastro"),
     meta: { title: 'Hortifruti - Cadastro' }
   },
+  {
+    path: '/minha-conta',
+    name: 'Minha-conta',
+    component: () => import("@/views/Minha-conta"),
+    meta: { title: 'Hortifruti - Minha Conta' }
+  },
 ]
 
 const router = new VueRouter({
